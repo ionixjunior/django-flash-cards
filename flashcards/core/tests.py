c874cc7a11e14833917ffe473779b0582ba72627
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 20:14:38 -0300

    Add test data setup for DeckListView tests

--- flashcards/core/tests.py
+++ flashcards/core/tests.py
@@ -1,5 +1,13 @@
 from unittest import TestCase
 
+from flashcards.core.models import Deck, Card
+
 
 class DeckListViewTest(TestCase):
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        Deck.objects.create(id=1, name="Test Deck 1")
+        Card.objects.create(deck_id=1, front="Test Front 1", back="Test Back 1")
+        Deck.objects.create(id=2, name="Test Deck 2")
+        Card.objects.create(deck_id=2, front="Test Front 2", back="Test Back 2")
+        Card.objects.create(deck_id=2, front="Test Front 3", back="Test Back 3")
