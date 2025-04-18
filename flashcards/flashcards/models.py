c4566135a9180ed3e5718e9483ec956bb48a413a
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 16:38:08 -0300

    Add Card model

--- flashcards/flashcards/models.py
+++ flashcards/flashcards/models.py
@@ -7,3 +7,14 @@ class Deck(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Card(models.Model):
+    deck_id = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    front = models.CharField(max_length=100)
+    back = models.TextField(max_length=100)
+    last_review_date = models.DateTimeField()
+    next_review_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.front} - {self.back}"
