f04fe1ec8e5a4f674b28bc60e403c2e5d28ecbe8
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 19:10:40 -0300

    Make last_studied, last_review_date, and next_review_date fields optional

--- flashcards/core/models.py
+++ flashcards/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Deck(models.Model):
     name = models.CharField(max_length=50)
-    last_studied = models.DateTimeField()
+    last_studied = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -13,8 +13,8 @@ class Card(models.Model):
     deck_id = models.ForeignKey(Deck, on_delete=models.CASCADE)
     front = models.CharField(max_length=100)
     back = models.CharField(max_length=100)
-    last_review_date = models.DateTimeField()
-    next_review_date = models.DateTimeField()
+    last_review_date = models.DateTimeField(null=True, blank=True)
+    next_review_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.front} - {self.back}"
