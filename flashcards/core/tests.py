cc2891668aa4337f23a6fea18859be11a379bc09
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 20:59:40 -0300

    Add test to verify last studied date of each deck in DeckListView

--- flashcards/core/tests.py
+++ flashcards/core/tests.py
@@ -1,3 +1,5 @@
+from datetime import datetime, UTC
+
 from django.test import TestCase, Client
 from django.urls import reverse
 
@@ -8,7 +10,7 @@ class DeckListViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         deck1 = Deck.objects.create(name="Test Deck 1")
-        deck2 = Deck.objects.create(name="Test Deck 2")
+        deck2 = Deck.objects.create(name="Test Deck 2", last_studied=datetime(2025, 4, 18, 12, 0, 0, tzinfo=UTC))
         Card.objects.create(deck_id=deck1, front="Test Front 1", back="Test Back 1")
         Card.objects.create(deck_id=deck2, front="Test Front 2", back="Test Back 2")
         Card.objects.create(deck_id=deck2, front="Test Front 3", back="Test Back 3")
@@ -37,3 +39,9 @@ class DeckListViewTest(TestCase):
         deck_cards_count = {deck.name: deck.card_set.count() for deck in response.context['decks']}
         self.assertEqual(deck_cards_count["Test Deck 1"], 1)
         self.assertEqual(deck_cards_count["Test Deck 2"], 2)
+
+    def test_decklist_when_accessed_should_list_last_studied_date_of_each_deck(self):
+        response = self.client.get(self.deck_list_url)
+        deck_last_studied = {deck.name: deck.last_studied for deck in response.context['decks']}
+        self.assertIsNone(deck_last_studied["Test Deck 1"])
+        self.assertEqual(deck_last_studied["Test Deck 2"], datetime(2025, 4, 18, 12, 0, 0, tzinfo=UTC))
