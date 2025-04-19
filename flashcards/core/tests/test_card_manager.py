22c710969ed8718a939493b93fba2d72cf8b27bb
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:25:30 -0300

    Arrange, act, assert changes

--- flashcards/core/tests/test_card_manager.py
+++ flashcards/core/tests/test_card_manager.py
@@ -7,7 +7,9 @@ from ..models import Deck, Card
 class CardManagerTest(TestCase):
     def test_next_card_should_be_none_when_no_cards_in_deck(self):
         deck = Deck.objects.create(name="Test Deck")
+
         next_card = Card.objects.next_card(deck)
+
         self.assertIsNone(next_card)
 
     def test_next_card_should_be_none_when_cards_in_deck_has_future_next_review_date(self):
