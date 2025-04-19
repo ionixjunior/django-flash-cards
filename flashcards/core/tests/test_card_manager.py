081b91b8be35c990c14c7f9e466767b17919aa18
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:25:54 -0300

    Arrange, act, assert changes

--- flashcards/core/tests/test_card_manager.py
+++ flashcards/core/tests/test_card_manager.py
@@ -16,5 +16,7 @@ class CardManagerTest(TestCase):
         next_review_date = datetime.today() + timedelta(days=1)
         deck = Deck.objects.create(name="Test Deck")
         Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=next_review_date)
+
         next_card = Card.objects.next_card(deck)
+        
         self.assertIsNone(next_card)
