bc0fe647bba21e058b51f5fc20caa4dde8b6bb38
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:45:01 -0300

    Update next_card method to include current date parameter

--- flashcards/core/managers.py
+++ flashcards/core/managers.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # pylint: disable=too-few-public-methods
 class CardManager(models.Manager):
-    def next_card(self, deck):
+    def next_card(self, current_date, deck):
         pass
