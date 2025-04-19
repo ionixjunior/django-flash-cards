bf32234e6b2fd4e1f431f81c650fd9ac8e06c8f5
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 23:51:25 -0300

    Refactor review date calculation to use feedback levels and intervals dict

--- flashcards/core/srs/simple_srs.py
+++ flashcards/core/srs/simple_srs.py
@@ -3,17 +3,25 @@ from datetime import timedelta
 
 # pylint: disable=too-few-public-methods
 class SimpleSRS:
+    FEEDBACK_LEVELS = {
+        'again': 0,
+        'hard': 1,
+        'good': 2,
+        'easy': 3,
+    }
+
+    INTERVALS = {
+        0: timedelta(days=0),
+        1: timedelta(days=1),
+        2: timedelta(days=3),
+        3: timedelta(days=7),
+    }
+
     def calculate_next_review_date(self, today, feedback):
-        if feedback == 'again':
-            return today + timedelta(days=0)
-
-        if feedback == 'hard':
-            return today + timedelta(days=1)
-
-        if feedback == 'good':
-            return today + timedelta(days=3)
-
-        if feedback == 'easy':
-            return today + timedelta(days=7)
-
-        return today + timedelta(days=0)
+        try:
+            level = self.FEEDBACK_LEVELS[feedback]
+            interval = self.INTERVALS[level]
+            next_date = today + interval
+            return next_date
+        except KeyError:
+            return today
