0a9ca70c668979dde7778aba15087fa98339a087
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:19:50 -0300

    Add test for next_card method in Card model when deck is empty

--- flashcards/core/tests/test_card_manager.py
+++ flashcards/core/tests/test_card_manager.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
 
+from ..models import Deck, Card
+
 class CardManagerTest(TestCase):
-    pass
+    def test_next_card_should_be_none_when_no_cards_in_deck(self):
+        deck = Deck.objects.create(name="Test Deck")
+        next_card = Card.objects.next_card(deck)
+        self.assertIsNone(next_card)
