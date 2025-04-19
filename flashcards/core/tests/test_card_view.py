77a7ab270b67653ec8f818a47dbaae91857c6482
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 22:23:15 -0300

    Rename property

--- flashcards/core/tests/test_card_view.py
+++ flashcards/core/tests/test_card_view.py
@@ -6,8 +6,8 @@ from django.urls import reverse
 class CardViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.deck_list_url = reverse('flash_card', args=[1])
+        self.flash_card_url = reverse('flash_card', args=[1])
 
     def test_card_view_when_accessed_should_return_200(self):
-        response = self.client.get(self.deck_list_url)
+        response = self.client.get(self.flash_card_url)
         self.assertEqual(response.status_code, 200)
