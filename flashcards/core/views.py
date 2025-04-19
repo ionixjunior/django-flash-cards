8392b78d2385ee7006b831fcb3ecc99d79dcd31e
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 12:57:18 -0300

    Update to save card review dates and feedback processing

--- flashcards/core/views.py
+++ flashcards/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 
 from .models import Deck, Card
+from .srs.simple_srs import SimpleSRS
 
 
 def deck_list(request):
@@ -15,6 +16,13 @@ def flash_card(request, deck_id):
     if request.method == "POST":
         deck.last_studied = timezone.now()
         deck.save()
+
+        card_id = request.POST.get("card_id")
+        feedback = request.POST.get("feedback")
+        card = Card.objects.get(pk=card_id)
+        card.last_review_date = timezone.now()
+        card.next_review_date = SimpleSRS().calculate_next_review_date(today=timezone.now(), feedback=feedback)
+        card.save()
 
     current_date = timezone.now()
     card = Card.objects.next_card(current_date=current_date, deck=deck)
