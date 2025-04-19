d13f4fa4087359b5847277bfffa8bb07fca82c23
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:40:57 -0300

    Fix pylint issues

--- flashcards/core/tests/test_card_manager.py
+++ flashcards/core/tests/test_card_manager.py
@@ -15,7 +15,10 @@ class CardManagerTest(TestCase):
     def test_next_card_should_be_none_when_cards_in_deck_has_future_next_review_date(self):
         next_review_date = datetime.today() + timedelta(days=1)
         deck = Deck.objects.create(name="Test Deck")
-        Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=next_review_date)
+        Card.objects.create(deck_id=deck,
+                            front="Test Front",
+                            back="Test Back",
+                            next_review_date=next_review_date)
 
         next_card = Card.objects.next_card(deck)
 
@@ -24,7 +27,10 @@ class CardManagerTest(TestCase):
     def test_next_card_should_return_card_with_today_next_review_date(self):
         today = datetime.today()
         deck = Deck.objects.create(name="Test Deck")
-        expected_card = Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=today)
+        expected_card = Card.objects.create(deck_id=deck,
+                                            front="Test Front",
+                                            back="Test Back",
+                                            next_review_date=today)
 
         next_card = Card.objects.next_card(deck)
 
@@ -33,7 +39,10 @@ class CardManagerTest(TestCase):
     def test_next_card_should_return_card_with_yesterday_next_review_date(self):
         yesterday = datetime.today() - timedelta(days=1)
         deck = Deck.objects.create(name="Test Deck")
-        expected_card = Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=yesterday)
+        expected_card = Card.objects.create(deck_id=deck,
+                                            front="Test Front",
+                                            back="Test Back",
+                                            next_review_date=yesterday)
 
         next_card = Card.objects.next_card(deck)
 
@@ -44,10 +53,21 @@ class CardManagerTest(TestCase):
         one_day_before = datetime.today() - timedelta(days=1)
         today = datetime.today()
         deck = Deck.objects.create(name="Test Deck")
-        expected_card = Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=two_days_before)
-        Card.objects.create(deck_id=deck, front="Test Front 2", back="Test Back 2", next_review_date=one_day_before)
-        Card.objects.create(deck_id=deck, front="Test Front 3", back="Test Back 3", next_review_date=today)
-        Card.objects.create(deck_id=deck, front="Test Front 4", back="Test Back 4")
+        expected_card = Card.objects.create(deck_id=deck,
+                                            front="Test Front",
+                                            back="Test Back",
+                                            next_review_date=two_days_before)
+        Card.objects.create(deck_id=deck,
+                            front="Test Front 2",
+                            back="Test Back 2",
+                            next_review_date=one_day_before)
+        Card.objects.create(deck_id=deck,
+                            front="Test Front 3",
+                            back="Test Back 3",
+                            next_review_date=today)
+        Card.objects.create(deck_id=deck,
+                            front="Test Front 4",
+                            back="Test Back 4")
 
         next_card = Card.objects.next_card(deck)
 
@@ -56,8 +76,13 @@ class CardManagerTest(TestCase):
     def test_next_card_should_return_unreviewed_card(self):
         one_day_later = datetime.today() + timedelta(days=1)
         deck = Deck.objects.create(name="Test Deck")
-        Card.objects.create(deck_id=deck, front="Test Front 1", back="Test Back 2", next_review_date=one_day_later)
-        expected_card = Card.objects.create(deck_id=deck, front="Test Front 2", back="Test Back 2")
+        Card.objects.create(deck_id=deck,
+                            front="Test Front 1",
+                            back="Test Back 2",
+                            next_review_date=one_day_later)
+        expected_card = Card.objects.create(deck_id=deck,
+                                            front="Test Front 2",
+                                            back="Test Back 2")
 
         next_card = Card.objects.next_card(deck)
 
