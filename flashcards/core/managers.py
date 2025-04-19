2042c2647fd788e1719c34658dedee126e44c39b
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:52:06 -0300

    Implement the query logic to order by the oldest card

--- flashcards/core/managers.py
+++ flashcards/core/managers.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.template.defaultfilters import first
 
 
 # pylint: disable=too-few-public-methods
 class CardManager(models.Manager):
     def next_card(self, current_date, deck):
-        pass
+        card_to_view = self.get_queryset().filter(
+            deck_id=deck
+        ).order_by(
+            'next_review_date'
+        ).first()
+
+        return card_to_view
