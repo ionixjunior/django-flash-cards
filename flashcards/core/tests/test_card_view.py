5718af87c6a6e631b442da23713099e817923c44
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 22:26:22 -0300

    Add unit tests for card view to check deck name

--- flashcards/core/tests/test_card_view.py
+++ flashcards/core/tests/test_card_view.py
@@ -2,12 +2,24 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from ..models import Deck, Card
+
 
 class CardViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.deck1 = Deck.objects.create(name="Test Deck 1")
+        Card.objects.create(deck_id=cls.deck1, front="Test Front 1", back="Test Back 1")
+
     def setUp(self):
         self.client = Client()
-        self.flash_card_url = reverse('flash_card', args=[1])
+        self.flash_card_url = reverse('flash_card', args=[self.deck1.pk])
 
     def test_card_view_when_accessed_should_return_200(self):
         response = self.client.get(self.flash_card_url)
         self.assertEqual(response.status_code, 200)
+
+    def test_card_view_when_accessed_should_have_deck_name(self):
+        response = self.client.get(self.flash_card_url)
+        self.assertTrue('deck_name' in response.context)
+        self.assertEqual(response.context['deck_name'], "Test Deck 1")
