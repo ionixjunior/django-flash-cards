36986421f33f37e7509b12a825528ae6b4316330
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:26:42 -0300

    Add test for next_card method to check card with today's next review date

--- flashcards/core/tests/test_card_manager.py
+++ flashcards/core/tests/test_card_manager.py
@@ -18,5 +18,15 @@ class CardManagerTest(TestCase):
         Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=next_review_date)
 
         next_card = Card.objects.next_card(deck)
-        
+
         self.assertIsNone(next_card)
+
+    def test_next_card_should_return_card_with_today_next_review_date(self):
+        today = datetime.today()
+        deck = Deck.objects.create(name="Test Deck")
+        Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=today)
+
+        next_card = Card.objects.next_card(deck)
+
+        self.assertIsNotNone(next_card)
+        self.assertEqual(next_card.id, deck.id)
