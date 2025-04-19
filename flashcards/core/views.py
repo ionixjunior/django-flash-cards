1cbeac82ec2db3657c42570d742d880bf5681334
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-18 22:01:08 -0300

    Add flash_card view to render flash card template

--- flashcards/core/views.py
+++ flashcards/core/views.py
@@ -6,3 +6,7 @@ from .models import Deck
 def deck_list(request):
     decks = Deck.objects.all()
     return render(request, "core/decks.html", {"decks": decks})
+
+
+def flash_card(request, deck_id):
+    return render(request, "core/flash_card.html")
