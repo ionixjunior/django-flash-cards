5cac67d39c824b68124690902c12a1647c9c390e
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 13:18:17 -0300

    Show feedback buttons when flash card is flipped

--- flashcards/core/static/core/js/flash_card.js
+++ flashcards/core/static/core/js/flash_card.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const flashcard = document.querySelector('.flash-card');
+    const feedbackButtons = document.querySelector('.feedback-buttons');
 
-    if (flashcard) {
+    if (flashcard && feedbackButtons) {
         flashcard.addEventListener('click', () => {
             if (!flashcard.classList.contains('is-flipped')) {
                 flashcard.classList.add('is-flipped');
+                feedbackButtons.style.display = 'block';
             }
         });
     }
