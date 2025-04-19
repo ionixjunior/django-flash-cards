e9b8386124f97f977d202283d9db8eb6bd27a6a3
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 00:23:24 -0300

    Add test for next_card method when cards have future next review date

--- flashcards/core/tests/test_card_manager.py
+++ flashcards/core/tests/test_card_manager.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.test import TestCase
 
 from ..models import Deck, Card
@@ -5,5 +7,12 @@ from ..models import Deck, Card
 class CardManagerTest(TestCase):
     def test_next_card_should_be_none_when_no_cards_in_deck(self):
         deck = Deck.objects.create(name="Test Deck")
+        next_card = Card.objects.next_card(deck)
+        self.assertIsNone(next_card)
+
+    def test_next_card_should_be_none_when_cards_in_deck_has_future_next_review_date(self):
+        next_review_date = datetime.today() + timedelta(days=1)
+        deck = Deck.objects.create(name="Test Deck")
+        Card.objects.create(deck_id=deck, front="Test Front", back="Test Back", next_review_date=next_review_date)
         next_card = Card.objects.next_card(deck)
         self.assertIsNone(next_card)
