03747854a61b1725e408536477b66edcfd5c972c
Ione Souza Junior <junior@ionixjunior.com.br>
2025-04-19 23:30:05 -0300

    Refactor feedback handling in SimpleSRS class

--- flashcards/core/srs/simple_srs.py
+++ flashcards/core/srs/simple_srs.py
@@ -3,24 +3,16 @@ from datetime import timedelta
 
 # pylint: disable=too-few-public-methods
 class SimpleSRS:
-    FEEDBACK_LEVELS = {
-        'again': 0,
-        'hard': 1,
-        'good': 2,
-        'easy': 3,
-    }
-
     INTERVALS = {
-        0: timedelta(minutes=1),
-        1: timedelta(days=1),
-        2: timedelta(days=3),
-        3: timedelta(days=7),
+        'again': timedelta(minutes=1),
+        'hard': timedelta(days=1),
+        'good': timedelta(days=3),
+        'easy': timedelta(days=7),
     }
 
     def calculate_next_review_date(self, today, feedback):
         try:
-            level = self.FEEDBACK_LEVELS[feedback]
-            interval = self.INTERVALS[level]
+            interval = self.INTERVALS[feedback]
             next_date = today + interval
             return next_date
         except KeyError:
